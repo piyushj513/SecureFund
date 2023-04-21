6c11870a773b2586f7e4d0e462a92281653c19b9
piyushj513 <piyushj513@gmail.com>
2023-04-21 16:08:18 +0530

    redirect

--- components/Metamask.js
+++ components/Metamask.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { useState } from 'react';
 import { ethers } from 'ethers';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Metamask = () => {
         const [address, setAddress] = useState("");
         const [balance, setBalance] = useState("");
-      
+        const router = useRouter();
       
         const connectWallet = async () => {
+          if(window.ethereum){
           await window.ethereum.request({ method: "eth_requestAccounts" });
           const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
           await window.ethereum.request({
             method: "wallet_requestPermissions",
             params: [{eth_accounts: {}}]
         });
-            const account = provider.getSigner();
-            const Address = await account.getAddress();
-            setAddress(Address);
-            const Balance = ethers.utils.formatEther(await account.getBalance());
-            setBalance(Balance);
-          
+        const account = provider.getSigner();
+        const Address = await account.getAddress();
+        setAddress(Address);
+        const Balance = ethers.utils.formatEther(await account.getBalance());
+        setBalance(Balance);
+          }
+          else{
+            router.push('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn')
+      }
         };
 
   return (
